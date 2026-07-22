c9efa728f703e4e233d536e9c6d4451606328d8b
migueltaranta12-beep <migueltaranta12@gmail.com>
2026-07-21 22:11:30 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Configuración de Firebase
+export const firebaseConfig = {
+  apiKey: "AIzaSyC8YBo6ynaldT7iRJ4fPLe_5Bxkj1o9_sc",
+  authDomain: "clicar-627e8.firebaseapp.com",
+  projectId: "clicar-627e8",
+  storageBucket: "clicar-627e8.firebasestorage.app",
+  messagingSenderId: "1038834599999",
+  appId: "1:1038834599999:web:51ea9a9669858caa93508a"
+};
+
+// Configuración de Cloudinary
+export const cloudinaryConfig = {
+  cloudName: "zpbpygfg",
+  uploadPreset: "Clicarautos"
+};
