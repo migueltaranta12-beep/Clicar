a9d1f853e9abb8155a46b7fff5502b560e09c393
migueltaranta12-beep <migueltaranta12@gmail.com>
2026-07-23 13:25:31 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,30 +1,32 @@
-import { firebaseConfig } from "./firebase.js";
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { auth } from "./firebase.js";
 
 import {
-  getAuth,
   signInWithEmailAndPassword,
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
 window.login = async () => {
+
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   try {
+
     await signInWithEmailAndPassword(auth, email, password);
     window.location.href = "admin.html";
+
   } catch (e) {
+
     alert("Correo o contraseña incorrectos.");
+
   }
+
 };
 
 onAuthStateChanged(auth, (user) => {
+
   if (user && window.location.pathname.includes("login.html")) {
     window.location.href = "admin.html";
   }
+
 });
